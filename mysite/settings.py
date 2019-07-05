e66908d3bc5e488db549c1fed49a87ab1002a4d0
Dhanny Tandil <dhanny.tandil@gmail.com>
2019-07-05 15:02:01 +0700

    Added fortuna on allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e)udn@jr^+bjmbi82dwr^gt8enic0-h=(-o92yt8!cn#jp=n3^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','.pythonanyhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','fortuna.pythonanyhere.com']
 
 
 # Application definition
